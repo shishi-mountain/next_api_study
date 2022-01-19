8dce740e506387f9bfde723881b7e93e397fd5d6
shishi-mountain <chiichiitei_0711@yahoo.co.jp>
2022-01-19 15:29:35 +0900

    apitest

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,35 @@
-export default function handler(req, res) {
-    res.status(200).json({ text: 'wao!!' })
+import {Sequelize, DataTypes} from 'sequelize';
+
+export default (req, res) => {
+  //コネクションの作成
+  const connection = new Sequelize(
+    "postgres",     //DB名
+    "aduser",     //ユーザー名
+    "test",     //パスワード
+    {
+      dialect: "postgres"  //DB製品名
+    }
+  );
+
+  //モデルの定義
+  const Mountain = connection.define("Mountains", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      comment: "ID"
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      comment: "名",
+    }
+  });
+
+  //モデルとDBの同期化
+  Mountain.sync();
+
+  //情報の取得
+  Mountain.findByPk(req.query.id).then(function(value) {
+    res.statusCode = 200
+    res.json(value)
+  });
 }
